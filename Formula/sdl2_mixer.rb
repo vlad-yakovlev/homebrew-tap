1b9960795b46b8804cde2a7717b00a0dd7e10d17
Vladislav Yakovlev <vlad309523@gmail.com>
2021-02-16 23:38:28 +0300

    Add sdl2_mixer bottle

---
+++ Formula/sdl2_mixer.rb
@@ -0,0 +1,17 @@
+class Sdl2Mixer < Formula
+  desc "Sample multi-channel audio mixer library"
+  homepage "https://www.libsdl.org/projects/SDL_mixer/"
+  url "https://github.com/vlad-yakovlev/homebrew-tap/raw/main/empty.tar.gz"
+  sha256 "84efc8f5fa0387200f7b073132d719379be9d9198f60f5a4d0c00fe5e1a2a018"
+  version "2.0.4"
+
+  bottle do
+    root_url "https://dl.bintray.com/vlad-yakovlev/bottles-tap"
+    sha256 cellar: :any, arm64_big_sur: "a9f3979f3480e1afaf4bdf6e7c8d2b8e00d3446840d75bb14b42788b6be55283"
+    sha256 cellar: :any, big_sur:       "a9f3979f3480e1afaf4bdf6e7c8d2b8e00d3446840d75bb14b42788b6be55283"
+  end
+
+  depends_on "libmodplug"
+  depends_on "libvorbis"
+  depends_on "sdl2"
+end
--- README.md
+++ README.md
@@ -13,3 +13,4 @@ brew tap vlad-yakovlev/tap
 `minizip` - minizip bottle for macOS Big Sur x86_64 & arm64
 `sdl2` - sdl2 bottle for macOS Big Sur x86_64 & arm64
 `sdl2_image` - sdl2_image bottle for macOS Big Sur x86_64 & arm64
+`sdl2_mixer` - sdl2_mixer bottle for macOS Big Sur x86_64 & arm64
